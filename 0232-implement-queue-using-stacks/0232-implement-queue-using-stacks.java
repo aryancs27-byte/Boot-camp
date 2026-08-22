5a59c59c5976a5b5af5f3a7d224ba9a05d200e45
Aryan A <aryan.cs27@iilm.edu>
2026-08-22 11:29:03 +0530

    Time: 0 ms (100%), Space: 42.8 MB (33.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,42 +1,82 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+//import java.util.ArrayDeque;
+//import java.util.Deque;
+
+//class MyQueue {
+   // private Deque<Integer> inStack;
+    //private Deque<Integer> outStack;
+
+    //public MyQueue() {
+      //  inStack = new ArrayDeque<>();
+        //outStack = new ArrayDeque<>();
+    //}
+    
+    //public void push(int x) {
+      //  inStack.push(x);
+    //}
+    
+    //public int pop() {
+      //  moveInToOut();
+        //return outStack.pop();
+    //}
+    
+    //public int peek() {
+      //  moveInToOut();
+        //return outStack.peek();
+    //}
+    
+    //public boolean empty() {
+      //  return inStack.isEmpty() && outStack.isEmpty();
+    //}
+
+    //private void moveInToOut() {
+      //  if (outStack.isEmpty()) {
+        //    while (!inStack.isEmpty()) {
+          //      outStack.push(inStack.pop());
+            //}
+        //}
+    //}
+//}
 
 class MyQueue {
-    private Deque<Integer> inStack;
-    private Deque<Integer> outStack;
+    private int[] inStack;
+    private int[] outStack;
+    private int inTop;
+    private int outTop;
 
     public MyQueue() {
-        inStack = new ArrayDeque<>();
-        outStack = new ArrayDeque<>();
+   
+        inStack = new int[100];
+        outStack = new int[100];
+        inTop = -1;
+        outTop = -1;
     }
     
     public void push(int x) {
-        inStack.push(x);
+        inStack[++inTop] = x;
     }
     
     public int pop() {
         moveInToOut();
-        return outStack.pop();
+        return outStack[outTop--];
     }
     
     public int peek() {
         moveInToOut();
-        return outStack.peek();
+        return outStack[outTop];
     }
     
     public boolean empty() {
-        return inStack.isEmpty() && outStack.isEmpty();
+        return inTop == -1 && outTop == -1;
     }
 
     private void moveInToOut() {
-        if (outStack.isEmpty()) {
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
+        if (outTop == -1) {
+            while (inTop >= 0) {
+                outStack[++outTop] = inStack[inTop--];
             }
         }
     }
 }
-
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
